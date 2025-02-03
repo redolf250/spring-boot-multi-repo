c0b7a4176a404108572d9858a150d1d4cf35eb43
redolf250 <55606159+redolf250@users.noreply.github.com>
2025-02-03 00:56:47 +0000

    initial commits

---
+++ stored-procedure/src/test/java/com/redolf/storedprocedure/StoredProcedureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redolf.storedprocedure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoredProcedureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
