f507f1f77895e7c929edbd5082a2cdac5ae965c4
redolf250 <55606159+redolf250@users.noreply.github.com>
2025-02-12 00:30:48 +0000

    initial commits

--- spring-ai/build.gradle.kts
+++ spring-ai/build.gradle.kts
@@ -22,11 +22,11 @@ extra["springAiVersion"] = "1.0.0-M5"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.ai:spring-ai-cassandra-store-spring-boot-starter")
+    //implementation("org.springframework.ai:spring-ai-cassandra-store-spring-boot-starter")
     implementation("org.springframework.ai:spring-ai-ollama-spring-boot-starter")
-    implementation("org.springframework.ai:spring-ai-pdf-document-reader")
-    implementation("org.springframework.ai:spring-ai-pgvector-store-spring-boot-starter")
-    implementation("org.springframework.ai:spring-ai-vertex-ai-gemini-spring-boot-starter")
+    //implementation("org.springframework.ai:spring-ai-pdf-document-reader")
+    //implementation("org.springframework.ai:spring-ai-pgvector-store-spring-boot-starter")
+    //implementation("org.springframework.ai:spring-ai-vertex-ai-gemini-spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- spring-session/src/main/java/com/redolf/springsession/config/SessionConfig.java
+++ spring-session/src/main/java/com/redolf/springsession/config/SessionConfig.java
@@ -1,9 +1,13 @@
 package com.redolf.springsession.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.SessionRepository;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
 
 @Configuration
 @EnableSpringHttpSession
 public class SessionConfig {
+
 }
